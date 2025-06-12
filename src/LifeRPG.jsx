8e4b98b4c611e115c5cddda8d8f0ca6b64530c41
jeonjunhee93 <lllllljz@naver.com>
2025-06-12 20:38:19 +0900

    Update LifeRPG.jsx

--- src/LifeRPG.jsx
+++ src/LifeRPG.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 import silhouette from "/src/silhouette.png"; // ✅ 실루엣 이미지 경로
 
 export default function LifeRPG() {
